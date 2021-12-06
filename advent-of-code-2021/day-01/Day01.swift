4cddf8ac127b8be8750980ae0346899584d8b040
Jeb Schiefer <jebschiefer@gmail.com>
2021-12-06 13:42:39 -0500

    Find number of depth increases

--- advent-of-code-2021/day-01/Day01.swift
+++ advent-of-code-2021/day-01/Day01.swift
@@ -8,14 +8,29 @@
 import Foundation
 
 class Day01 {
-    
+
     func run() {
         print("------")
         print("Day 01")
-        
+
         if let data = Utility.readFile(name: "day01-input") {
-            print(data)
+            let depths = data.split(separator: "\n")
+
+            var previousDepth = -1
+            var numberOfIncreases = 0
+
+            for depth in depths {
+                let currentDepth = Int(depth) ?? 0
+
+                if previousDepth > -1 && currentDepth > previousDepth {
+                    numberOfIncreases += 1
+                }
+
+                previousDepth = currentDepth
+            }
+
+            print("Answer: \(numberOfIncreases)")
         }
     }
-    
+
 }
