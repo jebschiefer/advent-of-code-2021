4ebc4c007f5b32432fe947cd1e54ee3a77ce9d89
Jeb Schiefer <jebschiefer@gmail.com>
2021-12-07 09:40:06 -0500

    Add day 2 part 2 - move submarine with aim

--- advent-of-code-2021/day-02/Day02.swift
+++ advent-of-code-2021/day-02/Day02.swift
@@ -13,15 +13,21 @@ class Day02 {
         print("------")
         print("Day 02")
 
-        let example = move(inputFile: "day02-example")
-        print("  Part one example: \(example.horizontal * example.depth)")
+        let partOneExample = move(withAim: false, inputFile: "day02-example")
+        print("  Part one example: \(partOneExample.horizontal * partOneExample.depth)")
 
-        let partOne = move(inputFile: "day02-input")
+        let partOne = move(withAim: false, inputFile: "day02-input")
         print("  Part one: \(partOne.horizontal * partOne.depth)")
+
+        let partTwoExample = move(withAim: true, inputFile: "day02-example")
+        print("  Part two example: \(partTwoExample.horizontal * partTwoExample.depth)")
+
+        let partTwo = move(withAim: true, inputFile: "day02-input")
+        print("  Part two: \(partTwo.horizontal * partTwo.depth)")
     }
 
-    func move(inputFile: String) -> (horizontal: Int, depth: Int) {
-        var position = (horizontal: 0, depth: 0)
+    func move(withAim: Bool, inputFile: String) -> (horizontal: Int, depth: Int, aim: Int) {
+        var position = (horizontal: 0, depth: 0, aim: 0)
 
         if let data = Utility.readFile(name: inputFile) {
             let commands = data.split(separator: "\n")
@@ -31,12 +37,23 @@ class Day02 {
                 let movement = parts[0]
                 let units = Int(parts[1]) ?? 0
 
-                if movement == "forward" {
-                    position.horizontal += units
-                } else if movement == "down" {
-                    position.depth += units
-                } else if movement == "up" {
-                    position.depth -= units
+                if withAim {
+                    if movement == "forward" {
+                        position.horizontal += units
+                        position.depth += (position.aim * units)
+                    } else if movement == "down" {
+                        position.aim += units
+                    } else if movement == "up" {
+                        position.aim -= units
+                    }
+                } else {
+                    if movement == "forward" {
+                        position.horizontal += units
+                    } else if movement == "down" {
+                        position.depth += units
+                    } else if movement == "up" {
+                        position.depth -= units
+                    }
                 }
             }
         }
